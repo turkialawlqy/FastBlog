e914929d8b0acbe00093066d677d888d896f19f4
Nguyen Van Hau <50167033+devhau@users.noreply.github.com>
2021-08-02 00:12:51 +0700

    add slug

---
+++ database/migrations/2021_08_01_150612_create_slugs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlugsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slugs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug');
+            $table->string('type');
+            $table->string('target');
+            $table->unsignedBigInteger('target_id');
+            $table->unsignedTinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slugs');
+    }
+}
